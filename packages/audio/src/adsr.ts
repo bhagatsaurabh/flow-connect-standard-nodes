f32c87b95ad4854a991e90f370d205c75b650bf2
Saurabh Bhagat <saurabhbhagat98die@gmail.com>
2023-05-27 21:05:16 +0530

    7 audio nodes

--- packages/audio/src/adsr.ts
+++ packages/audio/src/adsr.ts
@@ -1,7 +1,16 @@
-import { Flow, Vector, Node } from 'flow-connect/core';
-import { NodeCreatorOptions } from 'flow-connect/common';
-import { clamp, denormalize } from 'flow-connect/utils';
-import { Envelope, Input, InputType } from 'flow-connect/ui';
+import { Flow, Vector, Node, NodeOptions, TerminalType, NodeStyle } from "flow-connect/core";
+import { clamp, denormalize } from "flow-connect/utils";
+import {
+  Envelope,
+  EnvelopeOptions,
+  HorizontalLayout,
+  HorizontalLayoutOptions,
+  Input,
+  InputOptions,
+  InputType,
+  Label,
+  LabelOptions,
+} from "flow-connect/ui";
 
 export class ADSR extends Node {
   envelopeInput: Envelope;
@@ -12,128 +21,221 @@ export class ADSR extends Node {
   sInput: Input;
   rInput: Input;
 
+  get audioCtx(): AudioContext {
+    return this.flow.flowConnect.audioContext;
+  }
+
   proxyParamNode: AudioWorkletNode;
   proxyParam: AudioParam;
 
-  static DefaultState = { min: 0, max: 1, a: 0.4, d: 0.2, s: 0.6, r: 0.4, trigger: false };
+  private static DefaultState = {
+    min: 0,
+    max: 1,
+    a: 0.4,
+    d: 0.2,
+    s: 0.6,
+    r: 0.4,
+    trigger: false,
+  };
 
-  constructor(flow: Flow, options: NodeCreatorOptions = {}) {
-    super(flow, options.name || 'ADSR', options.position || new Vector(50, 50), options.width || 280,
-      [{ name: 'trigger', dataType: 'event' }],
-      [{ name: 'out', dataType: 'audio' }],
-      {
-        state: options.state ? { ...ADSR.DefaultState, ...options.state } : ADSR.DefaultState,
-        style: options.style || { rowHeight: 10, spacing: 15 },
-        terminalStyle: options.terminalStyle || {}
-      }
-    );
+  constructor(_flow: Flow, _options: ADSRNodeOptions) {
+    super();
+  }
 
-    this.proxyParamNode = new AudioWorkletNode(
-      flow.flowConnect.audioContext,
-      'proxy-param',
-      { numberOfOutputs: 1, parameterData: { param: 0 } }
-    );
-    this.proxyParam = (this.proxyParamNode.parameters as Map<string, AudioParam>).get('param');
+  protected setupIO(_options: ADSRNodeOptions): void {
+    this.addTerminal({
+      name: "trigger",
+      type: TerminalType.IN,
+      dataType: "event",
+    });
+    this.addTerminal({
+      name: "out",
+      type: TerminalType.OUT,
+      dataType: "audio",
+    });
+  }
+
+  protected created(options: ADSRNodeOptions): void {
+    const { state = {}, style = {}, name = "ADSR", width = 240 } = options;
+
+    this.name = name;
+    this.width = width;
+    this.state = { ...ADSR.DefaultState, ...state };
+    this.style = { ...DefaultADSRNodeStyle(), ...style };
+
+    this.proxyParamNode = new AudioWorkletNode(this.audioCtx, "proxy-param", {
+      numberOfOutputs: 1,
+      parameterData: { param: 0 },
+    });
+    this.proxyParam = (this.proxyParamNode.parameters as Map<string, AudioParam>).get("param");
     this.outputs[0].ref = this.proxyParamNode;
+
     this.setMinMax();
     this.state.s = clamp(this.state.s, 0, 1);
-
     this.setupUI();
-
-    this.minInput.on('blur', this.setMinMax);
-    this.maxInput.on('blur', this.setMinMax);
-    this.aInput.on('blur', () => this.adsrChanged());
-    this.dInput.on('blur', () => this.adsrChanged());
-    this.sInput.on('blur', () => this.adsrChanged());
-    this.rInput.on('blur', () => this.adsrChanged());
-
-    this.inputs[0].on('event', () => {
-      this.state.trigger = !this.state.trigger;
-      let currTime = flow.flowConnect.audioContext.currentTime;
-      let { a, d, s, r } = this.state;
-      if (this.state.trigger) {
-        // Attack
-        this.proxyParam.cancelScheduledValues(0);
-        this.proxyParam.setValueAtTime(this.state.min, currTime);
-        this.proxyParam.linearRampToValueAtTime(1 * this.state.max, currTime + a);
-        this.proxyParam.linearRampToValueAtTime(denormalize(s, this.state.min, this.state.max), currTime + a + d);
-      } else {
-        // Release
-        this.proxyParam.cancelScheduledValues(0);
-        this.proxyParam.setValueAtTime(this.proxyParam.value, currTime);
-        this.proxyParam.linearRampToValueAtTime(this.state.min, currTime + r);
-      }
-    });
-
-    this.handleAudioConnections();
+    this.setupListeners();
   }
-  adsrChanged() {
-    this.state.s = clamp(this.state.s, 0, 1);
-    let { a, d, r } = this.state;
-    let totalDur = a + d + r;
-    this.envelopeInput.value = [
-      Vector.Zero(), new Vector(a / totalDur, 1), new Vector((a + d) / totalDur, this.state.s), new Vector(1, 0)
-    ];
-  }
-  stopAutomation() {
-    this.proxyParam.cancelScheduledValues(this.flow.flowConnect.audioContext.currentTime);
-  }
+
+  protected process(_inputs: any[]): void {}
+
   setMinMax() {
     this.proxyParamNode.port.postMessage({
-      type: 'set-range',
-      value: { min: this.state.min, max: this.state.max }
+      type: "set-range",
+      value: { min: this.state.min, max: this.state.max },
     });
   }
   setupUI() {
     let { a: atck, d: dcay, r: rlse } = this.state;
     let duration = atck + dcay + rlse;
 
-    this.envelopeInput = this.createEnvelope(145, [
-      Vector.Zero(), new Vector(atck / duration, 1), new Vector((atck + dcay) / duration, this.state.s), new Vector(1, 0)
-    ], { style: { pointColor: '#fcba03' } });
+    this.envelopeInput = this.createUI<Envelope, EnvelopeOptions>("core/envelope", {
+      height: 145,
+      values: [
+        Vector.Zero(),
+        Vector.create(atck / duration, 1),
+        Vector.create((atck + dcay) / duration, this.state.s),
+        Vector.create(1, 0),
+      ],
+      style: { pointColor: "#fcba03" },
+    });
     this.envelopeInput.disabled = true;
 
-    this.minInput = this.createInput({
-      propName: 'min', height: 20, style: { type: InputType.Number, grow: .5, step: 'any' }
+    this.minInput = this.createUI<Input, InputOptions>("core/input", {
+      propName: "min",
+      height: 20,
+      style: { type: InputType.Number, grow: 0.5, step: "any" },
     });
-    this.maxInput = this.createInput({
-      propName: 'max', height: 20, style: { type: InputType.Number, grow: .5, step: 'any' }
+    this.maxInput = this.createUI<Input, InputOptions>("core/input", {
+      propName: "max",
+      height: 20,
+      style: { type: InputType.Number, grow: 0.5, step: "any" },
     });
-    this.aInput = this.createInput({ propName: 'a', height: 20, style: { type: InputType.Number, step: 'any', grow: .5, precision: 3 } });
-    this.dInput = this.createInput({ propName: 'd', height: 20, style: { type: InputType.Number, step: 'any', grow: .5, precision: 3 } });
-    this.sInput = this.createInput({ propName: 's', height: 20, style: { type: InputType.Number, step: 'any', grow: .5, precision: 3 } });
-    this.rInput = this.createInput({ propName: 'r', height: 20, style: { type: InputType.Number, step: 'any', grow: .5, precision: 3 } });
+    this.aInput = this.createUI<Input, InputOptions>("core/input", {
+      propName: "a",
+      height: 20,
+      style: { type: InputType.Number, grow: 0.5, step: "any", precision: 3 },
+    });
+    this.dInput = this.createUI<Input, InputOptions>("core/input", {
+      propName: "d",
+      height: 20,
+      style: { type: InputType.Number, grow: 0.5, step: "any", precision: 3 },
+    });
+    this.sInput = this.createUI<Input, InputOptions>("core/input", {
+      propName: "s",
+      height: 20,
+      style: { type: InputType.Number, grow: 0.5, step: "any", precision: 3 },
+    });
+    this.rInput = this.createUI<Input, InputOptions>("core/input", {
+      propName: "r",
+      height: 20,
+      style: { type: InputType.Number, grow: 0.5, step: "any", precision: 3 },
+    });
 
     this.ui.append([
       this.envelopeInput,
-      this.createHozLayout([
-        this.createLabel('Min'), this.minInput,
-        this.createLabel('Max'), this.maxInput
-      ], { style: { spacing: 5 } }),
-      this.createHozLayout([
-        this.createLabel('A'), this.aInput, this.createLabel('D'), this.dInput
-      ], { style: { spacing: 5 } }),
-      this.createHozLayout([
-        this.createLabel('S'), this.sInput, this.createLabel('R'), this.rInput
-      ], { style: { spacing: 5 } })
+      this.createUI<HorizontalLayout, HorizontalLayoutOptions>("core/x-layout", {
+        childs: [
+          this.createUI<Label, LabelOptions>("core/label", { text: "Min" }),
+          this.minInput,
+          this.createUI<Label, LabelOptions>("core/label", { text: "Max" }),
+          this.maxInput,
+        ],
+        style: { spacing: 5 },
+      }),
+      this.createUI<HorizontalLayout, HorizontalLayoutOptions>("core/x-layout", {
+        childs: [
+          this.createUI<Label, LabelOptions>("core/label", { text: "A" }),
+          this.aInput,
+          this.createUI<Label, LabelOptions>("core/label", { text: "D" }),
+          this.dInput,
+        ],
+        style: { spacing: 5 },
+      }),
+      this.createUI<HorizontalLayout, HorizontalLayoutOptions>("core/x-layout", {
+        childs: [
+          this.createUI<Label, LabelOptions>("core/label", { text: "S" }),
+          this.sInput,
+          this.createUI<Label, LabelOptions>("core/label", { text: "R" }),
+          this.rInput,
+        ],
+        style: { spacing: 5 },
+      }),
     ]);
   }
-  handleAudioConnections() {
-    // Handle actual webaudio node stuff
-    this.outputs[0].on('connect', (_inst, connector) => {
+  setupListeners() {
+    this.minInput.on("blur", this.setMinMax);
+    this.maxInput.on("blur", this.setMinMax);
+    this.aInput.on("blur", () => this.adsrChanged());
+    this.dInput.on("blur", () => this.adsrChanged());
+    this.sInput.on("blur", () => this.adsrChanged());
+    this.rInput.on("blur", () => this.adsrChanged());
+
+    this.inputs[0].on("event", () => {
+      this.state.trigger = !this.state.trigger;
+      let currTime = this.audioCtx.currentTime;
+      const { a, d, s, r } = this.state;
+      if (this.state.trigger) {
+        this.attack(a, d, s, currTime);
+      } else {
+        this.release(r, currTime);
+      }
+    });
+
+    // WebAudio connections
+    this.outputs[0].on("connect", (_inst, connector) => {
       if (connector.end.ref instanceof AudioParam) {
         // Need to do this else the value provided by the worklet node as param value is getting offset instead of overwrite
         let offset = Math.max(0, connector.end.ref.minValue);
         connector.end.ref.value = offset;
         if (offset !== 0) {
-          this.proxyParamNode.port.postMessage({ type: 'set-offset', value: offset });
+          this.proxyParamNode.port.postMessage({
+            type: "set-offset",
+            value: offset,
+          });
         }
       }
       this.outputs[0].ref.connect(connector.end.ref);
     });
-    this.outputs[0].on('disconnect', (_inst, _connector, _start, end) => {
+    this.outputs[0].on("disconnect", (_inst, _connector, _start, end) => {
       this.stopAutomation();
       this.outputs[0].ref.disconnect(end.ref);
     });
   }
+
+  attack(a: number, d: number, s: number, currTime: number) {
+    this.proxyParam.cancelScheduledValues(0);
+    this.proxyParam.setValueAtTime(this.state.min, currTime);
+    this.proxyParam.linearRampToValueAtTime(1 * this.state.max, currTime + a);
+    this.proxyParam.linearRampToValueAtTime(denormalize(s, this.state.min, this.state.max), currTime + a + d);
+  }
+  release(r: number, currTime: number) {
+    this.proxyParam.cancelScheduledValues(0);
+    this.proxyParam.setValueAtTime(this.proxyParam.value, currTime);
+    this.proxyParam.linearRampToValueAtTime(this.state.min, currTime + r);
+  }
+
+  adsrChanged() {
+    this.state.s = clamp(this.state.s, 0, 1);
+    const { a, d, r } = this.state;
+    const totalDur = a + d + r;
+
+    this.envelopeInput.value = [
+      Vector.Zero(),
+      Vector.create(a / totalDur, 1),
+      Vector.create((a + d) / totalDur, this.state.s),
+      Vector.create(1, 0),
+    ];
+  }
+  stopAutomation() {
+    this.proxyParam.cancelScheduledValues(this.audioCtx.currentTime);
+  }
 }
+
+export interface ADSRNodeOptions extends NodeOptions {}
+
+export interface ADSRNodeStyle extends NodeStyle {}
+
+const DefaultADSRNodeStyle = (): NodeStyle => ({
+  rowHeight: 10,
+  spacing: 15,
+});
--- packages/audio/src/source.ts
+++ packages/audio/src/source.ts
@@ -1,8 +1,8 @@
 import { Flow, Vector, Node } from "flow-connect/core";
 import { NodeCreatorOptions } from "flow-connect/common";
-import { FlowConnectState } from "flow-connect";
+import { FlowConnectState, SerializedNode } from "flow-connect";
 import { Log } from "flow-connect/utils";
-import { Toggle, Source as OGSource } from 'flow-connect/ui';
+import { Toggle, Source as OGSource } from "flow-connect/ui";
 
 export class Source extends Node {
   fileInput: OGSource;
@@ -15,65 +15,97 @@ export class Source extends Node {
   static DefaultState: any = { buffer: null, loop: true, prevChannelCount: -1 };
 
   constructor(flow: Flow, options: NodeCreatorOptions = {}) {
-    super(flow, options.name || 'Audio Source', options.position || new Vector(50, 50), options.width || 200,
+    super(
+      flow,
+      options.name || "Audio Source",
+      options.position || new Vector(50, 50),
+      options.width || 200,
       [
-        { name: 'array-buffer', dataType: 'event' }, { name: 'gain', dataType: 'audioparam' },
-        { name: 'detune', dataType: 'audioparam' }, { name: 'playback-rate', dataType: 'audioparam' }
+        { name: "array-buffer", dataType: "event" },
+        { name: "gain", dataType: "audioparam" },
+        { name: "detune", dataType: "audioparam" },
+        { name: "playback-rate", dataType: "audioparam" },
       ],
-      [{ name: 'out', dataType: 'audio' }, { name: 'ended', dataType: 'event' }],
+      [
+        { name: "out", dataType: "audio" },
+        { name: "ended", dataType: "event" },
+      ],
       {
-        state: options.state ? { ...Source.DefaultState, ...options.state } : Source.DefaultState,
+        state: options.state
+          ? { ...Source.DefaultState, ...options.state }
+          : Source.DefaultState,
         style: options.style || { rowHeight: 10, spacing: 15 },
-        terminalStyle: options.terminalStyle || {}
+        terminalStyle: options.terminalStyle || {},
       }
     );
 
     this.volumeGainNode = flow.flowConnect.audioContext.createGain();
     this.proxyParamSourceNode = new AudioWorkletNode(
       flow.flowConnect.audioContext,
-      'proxy-param-for-source',
+      "proxy-param-for-source",
       { numberOfOutputs: 2, parameterData: { detune: 0, playbackRate: 1 } }
     );
 
     this.inputs[1].ref = this.volumeGainNode.gain;
-    this.inputs[2].ref = (this.proxyParamSourceNode.parameters as Map<string, AudioParam>).get('detune');
-    this.inputs[3].ref = (this.proxyParamSourceNode.parameters as Map<string, AudioParam>).get('playbackRate');
-    this.inputs[1].on('data', (_, data) => typeof data === 'number' && (this.inputs[1].ref.value = data));
-    this.inputs[2].on('data', (_, data) => typeof data === 'number' && (this.inputs[2].ref.value = data));
-    this.inputs[3].on('data', (_, data) => typeof data === 'number' && (this.inputs[3].ref.value = data));
+    this.inputs[2].ref = (
+      this.proxyParamSourceNode.parameters as Map<string, AudioParam>
+    ).get("detune");
+    this.inputs[3].ref = (
+      this.proxyParamSourceNode.parameters as Map<string, AudioParam>
+    ).get("playbackRate");
+    this.inputs[1].on(
+      "data",
+      (_, data) => typeof data === "number" && (this.inputs[1].ref.value = data)
+    );
+    this.inputs[2].on(
+      "data",
+      (_, data) => typeof data === "number" && (this.inputs[2].ref.value = data)
+    );
+    this.inputs[3].on(
+      "data",
+      (_, data) => typeof data === "number" && (this.inputs[3].ref.value = data)
+    );
     this.outputs[0].ref = this.volumeGainNode;
 
     this.setupUI();
 
-    this.loopToggle.on('change', () => {
+    this.loopToggle.on("change", () => {
       if (this.state.loop) {
         this.stopSource();
         this.playSource();
       } else this.source && (this.source.loop = this.state.loop);
     });
-    this.inputs[0].on('event', (_, data) => {
+    this.inputs[0].on("event", (_, data) => {
       if (!(data instanceof ArrayBuffer)) {
-        Log.error('Data received on Audio Source Node is not of type ArrayBuffer');
+        Log.error(
+          "Data received on Audio Source Node is not of type ArrayBuffer"
+        );
         return;
       }
       this.processArrayBuffer(data);
     });
-    this.fileInput.on('change', (_inst, _oldVal: File, newVal: File) => this.processFile(newVal));
-    this.fileInput.on('upload', (_inst, _oldVal: File, newVal: File) => this.processFile(newVal));
+    this.fileInput.on("change", (_inst, _oldVal: File, newVal: File) =>
+      this.processFile(newVal)
+    );
+    this.fileInput.on("upload", (_inst, _oldVal: File, newVal: File) =>
+      this.processFile(newVal)
+    );
 
-    flow.flowConnect.on('start', () => this.playSource());
-    flow.flowConnect.on('stop', () => this.stopSource());
+    flow.flowConnect.on("start", () => this.playSource());
+    flow.flowConnect.on("stop", () => this.stopSource());
 
     this.handleAudioConnections();
   }
 
   propagateChannelChange(newNoOfChannels: number) {
     this.flow.executionGraph.propagate(this, (currNode: Node) => {
-      currNode.call('channel-count-change', newNoOfChannels);
+      currNode.call("channel-count-change", newNoOfChannels);
     });
   }
   async processFile(file: File) {
-    let cached = this.flow.flowConnect.arrayBufferCache.get(file.name + file.type);
+    let cached = this.flow.flowConnect.arrayBufferCache.get(
+      file.name + file.type
+    );
     if (!cached) {
       cached = await file.arrayBuffer();
       this.flow.flowConnect.arrayBufferCache.set(file.name + file.type, cached);
@@ -83,12 +115,17 @@ export class Source extends Node {
   async processArrayBuffer(arrayBuffer: ArrayBuffer) {
     let cached = this.flow.flowConnect.audioBufferCache.get(arrayBuffer);
     if (!cached) {
-      cached = await this.flow.flowConnect.audioContext.decodeAudioData(arrayBuffer);
+      cached = await this.flow.flowConnect.audioContext.decodeAudioData(
+        arrayBuffer
+      );
       this.flow.flowConnect.audioBufferCache.set(arrayBuffer, cached);
 
       // If no. of channels has been changed, start an event propagation that will notify
       // every node that has a direct/indirect connection in the graph from this node
-      if (this.state.prevChannelCount < 0 || this.state.prevChannelCount !== cached.numberOfChannels) {
+      if (
+        this.state.prevChannelCount < 0 ||
+        this.state.prevChannelCount !== cached.numberOfChannels
+      ) {
         this.propagateChannelChange(cached.numberOfChannels);
       }
       this.state.prevChannelCount = cached.numberOfChannels;
@@ -99,9 +136,15 @@ export class Source extends Node {
     this.playSource();
   }
   playSource() {
-    if (this.flow.flowConnect.state === FlowConnectState.Stopped || !this.state.buffer) return;
+    if (
+      this.flow.flowConnect.state === FlowConnectState.Stopped ||
+      !this.state.buffer
+    )
+      return;
 
-    let audioSource = new AudioBufferSourceNode(this.flow.flowConnect.audioContext);
+    let audioSource = new AudioBufferSourceNode(
+      this.flow.flowConnect.audioContext
+    );
     audioSource.buffer = this.state.buffer;
     audioSource.loop = this.state.loop;
     this.source = audioSource;
@@ -118,21 +161,38 @@ export class Source extends Node {
     }
   }
   setupUI() {
-    this.fileInput = this.createSource({ input: true, output: true, height: 25, style: { grow: .7 } });
-    this.loopToggle = this.createToggle({ propName: 'loop', input: true, output: true, height: 10, style: { grow: .2 } });
+    this.fileInput = this.createSource({
+      input: true,
+      output: true,
+      height: 25,
+      style: { grow: 0.7 },
+    });
+    this.loopToggle = this.createToggle({
+      propName: "loop",
+      input: true,
+      output: true,
+      height: 10,
+      style: { grow: 0.2 },
+    });
 
     this.ui.append([
-      this.createHozLayout([this.createLabel('File', { style: { grow: .3 } }), this.fileInput], { style: { spacing: 5 } }),
-      this.createHozLayout([this.createLabel('Loop ?'), this.loopToggle], { style: { spacing: 5 } })
+      this.createHozLayout(
+        [this.createLabel("File", { style: { grow: 0.3 } }), this.fileInput],
+        { style: { spacing: 5 } }
+      ),
+      this.createHozLayout([this.createLabel("Loop ?"), this.loopToggle], {
+        style: { spacing: 5 },
+      }),
     ]);
   }
   handleAudioConnections() {
     // Handle actual webaudio node stuff
-    this.outputs[0].on('connect', (_, connector) => {
-      this.state.buffer && this.propagateChannelChange(this.state.buffer.numberOfChannels);
+    this.outputs[0].on("connect", (_, connector) => {
+      this.state.buffer &&
+        this.propagateChannelChange(this.state.buffer.numberOfChannels);
       this.outputs[0].ref.connect(connector.end.ref);
     });
-    this.outputs[0].on('disconnect', (_inst, _connector, _start, end) => {
+    this.outputs[0].on("disconnect", (_inst, _connector, _start, end) => {
       this.outputs[0].ref.disconnect(end.ref);
     });
   }
